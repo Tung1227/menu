7bf9e08b96d7056b2820a6dce0dd05358ec6738a
Tung1227 <tungluong2706@gmail.com>
2023-08-01 16:32:24 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { getDatabase, ref, set } from "firebase/database";
+import { initializeApp } from "firebase/app";
+
 
 function App() {
+  const firebaseConfig = {
+    // ...
+    // The value of `databaseURL` depends on the location of the database
+    databaseURL: "https://nhat-quang-bd36d-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  };
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  set(ref(db, 'menu/' + '2'), {
+    username: 'tùng',
+    email: 'email',
+    profile_picture: 'imageUrl'
+  });
   return (
     <div className="App">
       <header className="App-header">
